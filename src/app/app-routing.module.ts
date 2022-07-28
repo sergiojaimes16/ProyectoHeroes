bf7a924283b4e06f3e25e121d08c671e414b88e9
sergiojaimes <sergioandresjaimespimiento@gmail.com>
2022-07-27 20:07:47 -0500

    agregado de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { EjemplosPipesComponent } from './ejemplos-pipes/ejemplos-pipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pipes', component: EjemplosPipesComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
