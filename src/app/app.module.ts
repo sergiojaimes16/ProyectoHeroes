2c0238ff2ebf8a2245f8f8e5030f804e71923155
sergiojaimes <sergioandresjaimespimiento@gmail.com>
2022-07-27 19:41:46 -0500

    primer comentario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { EjemplosPipesComponent } from './ejemplos-pipes/ejemplos-pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AboutComponent,
+    NavbarComponent,
+    EjemplosPipesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ejemplos-pipes/ejemplos-pipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplos-pipes',
+  templateUrl: './ejemplos-pipes.component.html',
+  styleUrls: ['./ejemplos-pipes.component.css']
+})
+export class EjemplosPipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
